0b838a1b6bfa7be25a1cc8a72885f108b19097d4
Patryk Pozniak <ppozniak95@gmail.com>
2023-01-14 21:44:40 +0000

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <div
         className={clsx(montserrat.variable, ubuntuMono.variable, "font-sans")}
