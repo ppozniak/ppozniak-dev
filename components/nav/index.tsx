e0abdebffc18772ed8d1e9cdc9b2c470dd194d21
Patryk Pozniak <ppozniak95@gmail.com>
2023-01-15 15:38:57 +0000

    Add WIP mobile menu

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,30 +1,45 @@
 import { Logo } from "components/logo";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import clsx from "clsx";
 
 // @TODO: Make it fixed? Or create a separate nav for that
-export const StaticNav = () => (
-  <div className="py-4 border-b border-opacity-30 border-pastel bg-pastel bg-opacity-10 shadow-xl">
-    <div className="container">
-      <nav className="flex justify-between items-center md:justify-start">
-        <div className="mr-8">
-          <Logo variation="inline" />
-        </div>
-        <button className="md:hidden basis-12 flex-shrink-0">
-          <Bars3Icon className="text-white" />
-        </button>
-        <ul className="items-center hidden md:flex">
-          {[1, 2, 3, 4].map((number) => (
-            <li key={number} className="grow">
-              <a
-                href={"#" + number.toString()}
-                className="py-4 px-8 lg:px-12 grow font-bold hover:opacity-70 transition-opacity"
-              >
-                Item
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+export const StaticNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((isOpen) => !isOpen);
+
+  return (
+    <div className="py-4 border-b border-opacity-30 border-pastel bg-pastel bg-opacity-10 shadow-xl">
+      <div className="container">
+        <nav className="flex justify-between items-center md:justify-start">
+          <div className="mr-8">
+            <Logo variation="inline" />
+          </div>
+          {/* @TODO: Finish this */}
+          <button className="hidden basis-12 flex-shrink-0" onClick={toggle}>
+            <Bars3Icon className="text-white" />
+            {isOpen && "open"}
+          </button>
+          <ul
+            className={clsx(
+              "items-center md:flex",
+              isOpen ? "flex" : "hidden"
+              // "fixed top-0 right-0 left-0 flex-col pointer-events-none"
+            )}
+          >
+            {[1, 2, 3, 4].map((number) => (
+              <li key={number} className="grow">
+                <a
+                  href={"#" + number.toString()}
+                  className="py-4 px-8 lg:px-12 grow font-bold hover:opacity-70 transition-opacity"
+                >
+                  Item
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
-);
+  );
+};
