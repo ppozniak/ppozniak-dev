6bc413773d16b56f45c89648667c427406ccf13c
Patryk Pozniak <ppozniak95@gmail.com>
2023-01-15 16:14:08 +0000

    WIP skills page

--- components/sections/skills/index.tsx
+++ components/sections/skills/index.tsx
@@ -1,35 +1,39 @@
+import { Button } from "components/button";
 import { Container } from "components/container";
 import { SectionHeading } from "components/section-heading";
 
 export const Skills = () => {
   return (
     // @TODO: Add background image
-    <section className="bg-yellow text-black min-h-screen">
+    <section className="bg-yellow text-black">
       <Container>
         <header className="flex flex-col md:flex-row items-center gap-8 mb-8">
           <SectionHeading
             className="lg:flex-shrink-0"
             heading="Skills and experiences"
           />
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-            vitae ab, fugiat quisquam exercitationem, natus suscipit sint, animi
-            totam ipsam tempora atque possimus.
-          </p>
+          {/* @TODO: Cloud words description */}
+          {/* <p className="text-sm">
+            <strong>Skilled at:</strong> HTML, CSS, JavaScript, Next.js,
+            Typescript, React, accessibility, RWD, SASS, GraphQL, Node.js,
+            Tailwind, single page applications, unit testing, task runners and
+            build tools.
+          </p> */}
         </header>
-        <p>
-          My skills Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-          ad voluptatem, minus nobis quaerat possimus nesciunt aperiam dicta
-          cumque veritatis aliquam dolor deserunt voluptas aliquid nam sed quae!
-          Facere soluta, est vel et suscipit deserunt itaque rerum eius libero
-          aliquid mollitia nulla dolorem nostrum quia nemo, cumque at ullam
-          quaerat aut laboriosam. Praesentium, dolorum quod. Iste dignissimos
-          minima dicta eveniet rerum, impedit maiores velit a eligendi magnam ab
-          iusto! Possimus fugit necessitatibus omnis fuga quis porro iusto
-          aspernatur, veritatis accusamus optio dolorum corporis culpa
-          repudiandae reiciendis nemo cupiditate, laudantium ea eaque nobis
-          quisquam consequuntur saepe tempora quod hic? Dolorum, corporis?
+        {/* @TODO: Build skills cloud */}
+        <p className="max-w-2xl">
+          <strong>Skilled at:</strong> HTML, CSS, JavaScript, Next.js,
+          Typescript, React, accessibility, RWD, SASS, GraphQL, Node.js,
+          Tailwind, single page applications, unit testing, task runners and
+          build tools.
         </p>
+
+        <footer className="flex justify-end items-center gap-x-4 text-sm mt-4">
+          {/* @TODO: A link */}
+          ...to view job history
+          <Button className="max-w-[200px]">Visit LinkedIn</Button>
+          {/* @TODO: Add CV */}
+        </footer>
       </Container>
     </section>
   );
